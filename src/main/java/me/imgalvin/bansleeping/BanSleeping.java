d6fa1b10be7f0a26e38f6068172a98a085e0adcb
GalvinPython <imgalvin@pm.me>
2026-01-25 13:08:55 +0000

    update to 26.1 snapshots

--- src/main/java/me/imgalvin/bansleeping/BanSleeping.java
+++ src/main/java/me/imgalvin/bansleeping/BanSleeping.java
@@ -2,13 +2,14 @@ package me.imgalvin.bansleeping;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 
 public class BanSleeping implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		EntitySleepEvents.ALLOW_SLEEPING.register((PlayerEntity player, BlockPos blockPos) -> new PlayerEntity.SleepFailureReason(Text.literal("Sleeping is banned!")));
+		EntitySleepEvents.ALLOW_SLEEPING.register((Player player, BlockPos blockPos) -> new ServerPlayer.BedSleepingProblem(Component.literal("Sleeping is banned!")));
 	}
 }
